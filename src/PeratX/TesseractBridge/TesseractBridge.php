ad622637352faa31015b5acafb9bd2305c53d531
PeratX <1215714524@qq.com>
2017-02-18 10:35:21 +0800

    Init commit

---
+++ src/PeratX/TesseractBridge/TesseractBridge.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * TesseractBridge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PeratX
+ */
+
+namespace PeratX\TesseractBridge;
+
+use sf\module\Module;
+use sf\util\Config;
+
+class TesseractBridge extends Module{
+
+	private static $tesseractExecutable;
+	private static $outputFile;
+
+	public function load(){
+		@mkdir($this->getDataFolder());
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML, ["tesseract-executable" => "tesseract"]);
+		self::$tesseractExecutable = $config->get("tesseract-executable", "tesseract");
+		self::$outputFile = $this->getDataFolder() . "out";
+	}
+
+	public function unload(){
+		@unlink(self::$outputFile . ".txt");
+	}
+	public static function readImage(string $file, string $options = "") : string{
+		@unlink(self::$outputFile . ".txt");
+		exec(self::$tesseractExecutable . " " . $file . " " . self::$outputFile . ($options == "") ? : " " . $options);
+		if(file_exists(self::$outputFile . ".txt")){
+			return @file_get_contents(self::$outputFile . ".txt");
+		}
+		return "";
+	}
+}
